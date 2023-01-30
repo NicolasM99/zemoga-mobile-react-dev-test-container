f45038716718d5fd5ee3ed442ad1121300a2f4bd
nicolas.moreno <nicolasmoreno.evolve@gmail.com>
2023-01-30 15:37:53 -0500

    Setted up docker but fast reload is not working

--- zemoga-mobile-react-dev-test/App.tsx
+++ zemoga-mobile-react-dev-test/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Change 4895848484</Text>
       <StatusBar style="auto" />
     </View>
   );
